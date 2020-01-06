d8bff409c9c8050080b53d4f24b1ba71db053fa5
ShawnTseng <shawntseng40@gmail.com>
2020-01-06 09:52:23 +0800

    add: content setting from route

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ function start(route, handle) {
         var pathName = url.parse(request.url).pathname;
         console.log(`Request for ${pathName} received.`);
 
-        route(handle, pathName);
-
         response.writeHead(200, { 'Content-Type': 'text/plain' });
-        response.write('Hello Shawn');
+        var content = route(handle, pathName);
+        response.write(content);
         response.end();
     }
 

7c60ba5373dd7af80c06867d4fdedfdb7881da1d
ShawnTseng <shawntseng40@gmail.com>
2020-01-07 16:07:34 +0800

    add: add file input

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,6 @@
 var exec = require('child_process').exec;
 var querystring = require('querystring');
+var fs = require('fs');
 
 function start(response, postData) {
     console.log("Request handler 'start' was called.");
@@ -10,8 +11,8 @@ function start(response, postData) {
         'charset=UTF-8" />' +
         '</head>' +
         '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<form action="/upload" enctype="multipart/form-data" method="post">' +
+        '<input type="file" name="upload"' +
         '<input type="submit" value="Submit text" />' +
         '</form>' +
         '</body>' +
@@ -29,5 +30,21 @@ function upload(response, postData) {
     response.end();
 }
 
+function show(response, postData) {
+    console.log(`Request handler 'show' was called.`);
+    fs.readFile('/tmp/test.png', 'binary', function (error, file) {
+        if (error) {
+            response.writeHead(500, { 'Content-Type': 'text/plain' });
+            response.write(error + '\n');
+            response.end();
+        } else {
+            response.writeHead(200, { 'Content-Type': 'image/png' });
+            response.write(file, 'binary');
+            response.end();
+        }
+    })
+}
+
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
